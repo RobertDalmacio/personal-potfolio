f22343e24cf1d7c6016b9f91de36764a9d8fbd5e
Robert Dalmacio <rjohndalmacio@hotmail.com>
2022-08-04 14:30:56 -0400

    Updated Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFilCodepenCircle, AiFillLinkedin, AiFillCodepenCircle } from 'react-icons/ai';
 import { IoNewspaperOutline } from 'react-icons/io5';
 import { DiCssdeck } from 'react-icons/di';
 
@@ -11,7 +11,7 @@ const Header = () =>  (
     <Div1>
       <Link href='/'>
         <a style={{display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20px'}}>
-          <DiCssdeck size='3rem' /> <Span>Portfolio</Span>
+          <DiCssdeck size='3rem' /> <Span>Robert Dalmacio</Span>
         </a>
       </Link>
     </Div1>
@@ -33,14 +33,14 @@ const Header = () =>  (
       </li>
     </Div2>
     <Div3>
-      <SocialIcons href='https://github.com/RobertDalmacio'>
+      <SocialIcons href='https://github.com/RobertDalmacio' target='_blank'>
         <AiFillGithub size='3rem'/>
       </SocialIcons>
-      <SocialIcons href='https://linkedin.com'>
+      <SocialIcons href='https://linkedin.com' target='_blank'>
         <AiFillLinkedin size='3rem'/>
       </SocialIcons>
-      <SocialIcons href='https://linkedin.com'>
-        <IoNewspaperOutline size='3rem'/>
+      <SocialIcons href='https://codepen.io/RJDAL' target='_blank'>
+      <AiFillCodepenCircle size='3rem' />
       </SocialIcons>
     </Div3>
   </Container>

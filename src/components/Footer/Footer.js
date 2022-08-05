821a230feaee503407d0ef0ddbff555c400ac980
Robert Dalmacio <rjohndalmacio@hotmail.com>
2022-08-05 17:58:04 -0400

    Completed Footer and Added Head

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillCodepenCircle, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href='https://github.com/RobertDalmacio' target='_blank'>
+            <AiFillGithub size='3rem'/>
+          </SocialIcons>
+          <SocialIcons href='https://linkedin.com' target='_blank'>
+            <AiFillLinkedin size='3rem'/>
+          </SocialIcons>
+          <SocialIcons href='https://codepen.io/RJDAL' target='_blank'>
+            <AiFillCodepenCircle size='3rem' />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
+import {useEffect} from 'react';
+import Head from 'next/head'
 import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
@@ -6,10 +7,23 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+import {BsArrowUpSquare} from 'react-icons/bs'
+import { FaUser } from 'react-icons/fa';
 
 const Home = () => {
+  useEffect(() => {
+    // 👇️ scroll to top on page load
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
+  
   return (
     <Layout>
+      <Head>
+        <title>Robert Dalmacio Portfolio</title>
+        <meta name="description" content="Robert Dalmacio Portfolio" />
+        <link rel="icon" href='/RD-logo.png' />
+      </Head>
+
       <Section grid>
         <Hero />
         <BgAnimation />
@@ -17,7 +31,25 @@ const Home = () => {
       <Technologies />
       <Timeline />
       <Projects />
-      <Acomplishments />
+      {/* <Acomplishments /> */}
+      <button
+        onClick={() => {
+          window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+        }}
+        style={{
+          position: 'fixed',
+          padding: '0.5rem 0.75rem',
+          fontSize: '20px',
+          bottom: '40px',
+          right: '40px',
+          backgroundColor: '#0C9',
+          color: '#fff',
+          textAlign: 'center',
+          cursor: 'pointer'
+        }}
+      >
+        <BsArrowUpSquare size='3rem' />
+      </button>
     </Layout>
   );
 };
